bbb5ff296ef95a12edd8431dbf183fb1c72e9f20
bariabdul <abdul.bari.1@outlook.com>
2018-02-02 19:25:10 -0800

    added profile view

---
+++ Smack/Smack/Controller/ProfileVC.swift
@@ -0,0 +1,62 @@
+//
+//  ProfileVCViewController.swift
+//  Smack
+//
+//  Created by Bari Abdul on 2/2/18.
+//  Copyright © 2018 Bari Abdul. All rights reserved.
+//
+
+import UIKit
+
+class ProfileVC: UIViewController {
+
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var userName: UILabel!
+    @IBOutlet weak var userEmail: UILabel!
+    @IBOutlet weak var bgView: UIView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        setupView()
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func closeModalPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func logoutPressed(_ sender: Any) {
+        UserDataService.instance.logoutUser()
+        NotificationCenter.default.post(name: NOTIF_USER_DATA_DID_CHANGE, object: nil)
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func setupView() {
+        userName.text = UserDataService.instance.name
+        userEmail.text = UserDataService.instance.email
+        profileImage.image = UIImage(named: UserDataService.instance.avatarName)
+        profileImage.backgroundColor = UserDataService.instance.returnUIColor(components: UserDataService.instance.avatarColor)
+        
+        let closeTouch = UITapGestureRecognizer(target: self, action: #selector(ProfileVC.closeTap(_:)))
+        bgView.addGestureRecognizer(closeTouch)
+    }
+    
+    @objc func closeTap(_ recognizer: UITapGestureRecognizer) {
+        dismiss(animated: true, completion: nil)
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
