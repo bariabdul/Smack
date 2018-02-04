9f434564ccb48e50f0d41681baeab8a683afe488
bariabdul <abdul.bari.1@outlook.com>
2018-02-03 19:39:23 -0800

    view for the ability to add channels added

---
+++ Smack/Smack/Controller/AddChannelVC.swift
@@ -0,0 +1,58 @@
+//
+//  AddChannelVC.swift
+//  Smack
+//
+//  Created by Bari Abdul on 2/3/18.
+//  Copyright © 2018 Bari Abdul. All rights reserved.
+//
+
+import UIKit
+
+class AddChannelVC: UIViewController {
+
+    @IBAction func closeModalPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @IBOutlet weak var nameText: UITextField!
+    @IBOutlet weak var channelDesc: UITextField!
+    @IBOutlet weak var bgView: UIView!
+    
+    @IBAction func createChannelPressed(_ sender: Any) {
+    
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        setupView()
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func setupView() {
+        let closeTouch = UITapGestureRecognizer(target: self , action: #selector(AddChannelVC.closeTap(_:)))
+        
+        bgView.addGestureRecognizer(closeTouch)
+        
+        nameText.attributedPlaceholder = NSAttributedString(string: "name", attributes: [NSAttributedStringKey.foregroundColor: purplePlaceHolder])
+        channelDesc.attributedPlaceholder = NSAttributedString(string: "description", attributes: [NSAttributedStringKey.foregroundColor: purplePlaceHolder])
+    }
+
+    @objc func closeTap(_ recognizer: UITapGestureRecognizer) {
+        dismiss(animated: true, completion: nil)
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
